e3d2b669a0db6e7840ce6b0bef31e34d3a69bad6
RayTaylorLin <raytaylorlin@gmail.com>
2014-07-08 16:02:33 +0800

    添加选择歌曲方法

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,68 +2,72 @@ $(document).ready(function() {
     var IS_DEBUG = true;
 
     var $ghostPlayer = $("#ichangge-player"),
-        $mainPlayer = $("#ichangge-player-container");
+        $mainPlayer = $("#ichangge-player-container"),
+        myPlayList;
 
     changePlayButtonStyle(true);
     $mainPlayer.find('.jp-pause').hide();
 
-    $ghostPlayer.jPlayer({
-        ready: function(event) {
-            $(this).jPlayer("setMedia", {
-                title: "庞麦郎-我的滑板鞋",
-                mp3: "http://ichangge-player-music.qiniudn.com/mp3/%E5%BA%9E%E9%BA%A6%E9%83%8E-%E6%88%91%E7%9A%84%E6%BB%91%E6%9D%BF%E9%9E%8B.mp3",
-                oga: "http://ichangge-player-music.qiniudn.com/ogg/%E5%BA%9E%E9%BA%A6%E9%83%8E-%E6%88%91%E7%9A%84%E6%BB%91%E6%9D%BF%E9%9E%8B.ogg"
-            });
-        },
-        swfPath: "scripts/lib/",
-        solution: "html, flash",
-        // solution: "flash",
-        supplied: "mp3, oga",
-        volume: 0.8,
-        wmode: "window",
-        cssSelectorAncestor: "#ichangge-player-container",
-        smoothPlayBar: true,
-        keyEnabled: true,
-        remainingDuration: true,
-        toggleDuration: true,
-        loadstart: function(e) {
-            log('Start loading...');
-            if(navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
-                console.log('detected iphone');
+    $.getJSON('/playlist.json', function(data) {
+        console.log(data);
+        myPlayList = new jPlayerPlaylist({
+            jPlayer: '#ichangge-player',
+            cssSelectorAncestor: '#ichangge-player-container'
+        }, data, {
+            swfPath: "scripts/lib/",
+            solution: "html, flash",
+            // solution: "flash",
+            supplied: "mp3, oga",
+            volume: 0.8,
+            wmode: "window",
+            cssSelectorAncestor: "#ichangge-player-container",
+            fullScreen: true,
+            smoothPlayBar: true,
+            keyEnabled: true,
+            remainingDuration: true,
+            toggleDuration: true,
+            ready: function(e) {
+                selectSong(0);
+            },
+            loadstart: function(e) {
+                log('Start loading...');
+                if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+                    console.log('detected iphone');
+                    changePlayButtonStyle(true);
+                } else {
+                    changePlayButtonStyle(false);
+                }
+            },
+            error: function(e) {
+                log('error');
+                log(e.toString());
+                console.debug(e);
+            },
+            loadeddata: function(event) {
+                log('loadeddata');
+            },
+            loadedmetadata: function(e) {
+                log('loadedmetadata');
+            },
+            canplaythrough: function(e) {
+                log('canplaythrough');
+            },
+            canplay: function(e) {
+                log('canplay');
                 changePlayButtonStyle(true);
-            } else {
+            },
+            play: function(e) {
+                log('play');
                 changePlayButtonStyle(false);
+            },
+            pause: function(e) {
+                log('pause');
+                changePlayButtonStyle(true);
+            },
+            durationchange: function(e) {
+                log('duration change');
             }
-        },
-        error: function(e) {
-            log('error');
-            log(e.toString());
-            console.debug(e);
-        },
-        loadeddata: function(event) {
-            log('loadeddata');
-        },
-        loadedmetadata: function(e) {
-            log('loadedmetadata');
-        },
-        canplaythrough: function(e) {
-            log('canplaythrough');
-        },
-        canplay: function(e) {
-            log('canplay');
-            changePlayButtonStyle(true);
-        },
-        play: function(e) {
-            log('play');
-            changePlayButtonStyle(false);
-        },
-        pause: function(e) {
-            log('pause');
-            changePlayButtonStyle(true);
-        },
-        durationchange: function(e) {
-            log('duration change');
-        }
+        });
     });
 
     function changePlayButtonStyle(canPlay) {
@@ -74,6 +78,23 @@ $(document).ready(function() {
             $mainPlayer.find('.jp-play').find('i.fa-play')
                 .removeClass('fa-play').addClass('fa-spinner fa-spin');
         }
+    }
+
+    function selectSong(index) {
+        var currentSong;
+        if (index === 'next') {
+            myPlayList.next();
+        } else if (index === 'previous') {
+            myPlayList.previous();
+        } else {
+            myPlayList.select(index);
+        }
+        currentSong = myPlayList.playlist[myPlayList.current];
+        console.log(currentSong);
+    }
+
+    function resetDetail(song) {
+        $mainPlayer
     }
 
     function log(msg) {
